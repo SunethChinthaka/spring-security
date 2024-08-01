908b8d60955962447d751a00360d888118b611f1
SunethChinthaka <sunethedu@gmail.com>
2024-08-01 20:36:56 +0530

    Part 2 Custom Login

--- src/main/java/com/suneth/spring_security/HelloController.java
+++ src/main/java/com/suneth/spring_security/HelloController.java
@@ -1,13 +1,14 @@
 package com.suneth.spring_security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("")
-    public String greeting(){
-        return "Welcome to Spring Security";
+    @GetMapping("/")
+    public String greeting(HttpServletRequest request){
+        return "Welcome to Spring Security! Session ID: " + request.getSession().getId();
     }
 }
