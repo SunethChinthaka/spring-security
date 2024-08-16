bc978a19db76f618cf977d14ab4192bdb77dd7a9
SunethChinthaka <sunethedu@gmail.com>
2024-08-16 20:55:07 +0530

    Part 7 JWT Setup

--- src/main/java/com/suneth/spring_security/config/SecurityConfig.java
+++ src/main/java/com/suneth/spring_security/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.suneth.spring_security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,7 +28,10 @@ public class SecurityConfig {
 
         return httpSecurity
                 .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register", "login")
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
@@ -45,6 +50,11 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService);
 
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
    /* @Bean
--- src/main/java/com/suneth/spring_security/service/UserService.java
+++ src/main/java/com/suneth/spring_security/service/UserService.java
@@ -3,6 +3,9 @@ package com.suneth.spring_security.service;
 import com.suneth.spring_security.model.Users;
 import com.suneth.spring_security.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,23 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user) {
         user.setPassword(encoder.encode(user.getPassword()));
-       return userRepo.save(user);
+        return userRepo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+
+        if (authentication.isAuthenticated())
+            return "Login successful";
+        return "Login failed";
     }
 }
