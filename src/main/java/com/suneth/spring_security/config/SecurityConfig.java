d3d089c15bc208d13c79247e513b297c808ae9d1
SunethChinthaka <sunethedu@gmail.com>
2024-08-17 12:17:55 +0530

    Part 9.1 added proper comments

--- src/main/java/com/suneth/spring_security/config/SecurityConfig.java
+++ src/main/java/com/suneth/spring_security/config/SecurityConfig.java
@@ -58,19 +58,29 @@ public class SecurityConfig {
     }
 
     /**
-     * Un-authenticated Object -> Authentication Provider -> Authenticated Object
+     * Configures the AuthenticationProvider to handle authentication logic.
+     *
+     * @return A configured {@link AuthenticationProvider} that uses a DAO for authentication.
      */
-
     @Bean
     public AuthenticationProvider authenticationProvider() {
         //looking for a class which implements AuthenticationProvider
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        // Set the password encoder to bcrypt with a strength of 12 for hashing passwords
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        // Set the UserDetailsService to provide user-specific data during authentication
         provider.setUserDetailsService(userDetailsService);
 
         return provider;
     }
 
+    /**
+     * Provides an AuthenticationManager bean for handling authentication.
+     *
+     * @param configuration The {@link AuthenticationConfiguration} object used to obtain the AuthenticationManager.
+     * @return An {@link AuthenticationManager} bean.
+     * @throws Exception If an error occurs while creating the AuthenticationManager.
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
