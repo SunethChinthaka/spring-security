7f5fbec4dafab237068ade1a8e50280fdd1bdbb4
SunethChinthaka <sunethedu@gmail.com>
2024-08-17 13:37:12 +0530

    Part 10 OAuth2.0 Login with Google and GitHub

--- src/main/java/com/suneth/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/suneth/spring_security/SpringSecurityApplication.java
@@ -1,5 +1,6 @@
 package com.suneth.spring_security;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.load();
+
+		// Optionally, set the environment variables in the system properties
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+		System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
+
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
